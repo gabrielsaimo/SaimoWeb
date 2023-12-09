aafac60c79aeb8f5af84e6b0ff4688ae4bb540a0
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-12-09 01:25:03 -0300

    Add drawer menu to Home component

--- src/pages/modules/Home.jsx
+++ src/pages/modules/Home.jsx
@@ -1,58 +1,120 @@
-import React from "react";
-import { Layout, Menu, Typography, Button } from "antd";
+import React, { useState } from "react";
+import { Layout, Menu, Typography, Button, Drawer } from "antd";
+
+import { MenuOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import logo from "../../assets/saimo.webp";
 const { Header, Footer, Content } = Layout;
 const { Title, Paragraph } = Typography;
 
-const Home = () => (
-  <Layout style={{ minHeight: "100vh" }}>
-    <Header style={{ position: "fixed", zIndex: 1, width: "100%", height: 80 }}>
-      <div style={{ float: "left" }}>
-        <Link to="/">
-          <img
-            src={logo}
-            style={{ borderRadius: "100%", margin: "5px 30px 5px 10px" }}
-            alt="SaimoWeb"
-            height="64"
-          />
-        </Link>
-      </div>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["1"]}
-        style={{ margin: 5 }}
+const Home = () => {
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header
+        style={{ position: "fixed", zIndex: 1, width: "100%", height: 80 }}
       >
-        <Menu.Item key="1" style={{ borderRadius: 5 }}>
-          Opção 1
-        </Menu.Item>
-        <Menu.Item key="2" style={{ borderRadius: 5 }}>
-          Opção 2
-        </Menu.Item>
-        <Menu.Item key="3" style={{ borderRadius: 5 }}>
-          Opção 3
-        </Menu.Item>
-      </Menu>
-      <div style={{ position: "absolute", right: 10, top: 5 }}>
-        <Button type="primary" style={{ marginRight: "10px" }}>
-          <Link to="/login">Login</Link>
-        </Button>
-        <Button type="primary">
-          <Link to="/register">Registro</Link>
-        </Button>
-      </div>
-    </Header>
-    <Content style={{ padding: "0 50px", flex: 1, marginTop: 70 }}>
-      <Title>Criamos sites e aplicativos</Title>
-      <Paragraph>
-        Descrição detalhada sobre os serviços oferecidos pela SaimoWeb.
-      </Paragraph>
-    </Content>
-    <Footer style={{ textAlign: "center" }}>
-      © {new Date().getFullYear()} SaimoWeb. Todos os direitos reservados.
-    </Footer>
-  </Layout>
-);
+        <div style={{ float: "left" }}>
+          <Link to="/">
+            <img
+              src={logo}
+              style={{ borderRadius: "100%", margin: "5px 30px 5px 10px" }}
+              alt="SaimoWeb"
+              height="64"
+            />
+          </Link>
+        </div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          style={{ margin: 5 }}
+        >
+          <Menu.Item
+            key="1"
+            style={{
+              borderRadius: 5,
+              position: "absolute",
+              right: 10,
+              top: 5,
+              width: 80,
+
+              paddingRight: 10,
+              paddingTop: 10,
+              textAlign: "center",
+            }}
+            onClick={showDrawer}
+            icon={<MenuOutlined style={{ fontSize: 25 }} />}
+          />
+        </Menu>
+        <Drawer
+          title="SaimoWeb"
+          placement="right"
+          closable={false}
+          onClose={onClose}
+          open={visible}
+          extra={
+            <Button type="dashed" onClick={onClose}>
+              X
+            </Button>
+          }
+        >
+          <div>
+            <Button
+              type="primary"
+              style={{ marginRight: "10px", width: 150, marginBottom: 20 }}
+            >
+              <Link to="/login">Login</Link>
+            </Button>
+          </div>
+          <div>
+            <Button
+              type="primary"
+              style={{ marginRight: "10px", width: 150, marginBottom: 20 }}
+            >
+              <Link to="/register">Registro</Link>
+            </Button>
+          </div>
+        </Drawer>
+      </Header>
+      <Content style={{ padding: "0 50px", marginTop: 64 }}>
+        <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
+          <Title>Bem-vindo à SaimoWeb</Title>
+          <Paragraph>
+            A SaimoWeb é uma empresa líder em desenvolvimento de sites e
+            aplicativos. Com uma equipe de especialistas altamente qualificados,
+            oferecemos soluções modernas e inovadoras para ajudar nossos
+            clientes a alcançar seus objetivos de negócios.
+          </Paragraph>
+          <Title level={2}>Tecnologias de Programação</Title>
+          <Paragraph>
+            Utilizamos as mais recentes tecnologias de programação para garantir
+            que nossos produtos sejam de alta qualidade e estejam à frente da
+            curva. Nossas tecnologias incluem React, Node.js, Python, Java e
+            muito mais.
+          </Paragraph>
+          <Title level={2}>Soluções Modernas</Title>
+          <Paragraph>
+            Entendemos que o mundo da tecnologia está sempre mudando. É por isso
+            que nos esforçamos para fornecer soluções modernas que sejam
+            relevantes no mercado atual. Seja através do uso de inteligência
+            artificial, aprendizado de máquina ou desenvolvimento de aplicativos
+            móveis, estamos sempre buscando maneiras de inovar e melhorar.
+          </Paragraph>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        © {new Date().getFullYear()} SaimoWeb. Todos os direitos reservados.
+      </Footer>
+    </Layout>
+  );
+};
 
 export default Home;
